60d8e5dae1de7f89161b59a48cd2b93cffeb77f2
Muhammad Mujtaba Appiah <mujappiah@gmail.com>
2022-12-19 21:18:32 +0000

    changes

--- lib/theme/theme.dart
+++ lib/theme/theme.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 
+Color primaryColor = const Color(0x00000000);
+
+Map<int, Color> colors = {
+  for (var item in List.generate(10, (index) {
+    return index == 0 ? 50 : index * 100;
+  }))
+    item: primaryColor.withOpacity(item == 50 ? 0.1 : item / 1000 + 0.1)
+};
+MaterialColor primarySwatch = MaterialColor(0xFF880E4F, colors);
+
 ThemeData light = ThemeData(
   primaryColor: const Color(0x00000000),
+  primarySwatch: primarySwatch,
   fontFamily: 'Comic',
 );
